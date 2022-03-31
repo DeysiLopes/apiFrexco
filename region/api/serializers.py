136a0804cfe7256592c62605f22400a0e6f0bc37
DeysiLopes <deysilinconl@gmail.com>
2022-03-31 08:46:34 -0300

    Revert "Create Region"
    This reverts commit 6df3df3804c5b701cd48285c359d3409732b1440.

--- region/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from region import models
-
-class RegionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Region 
-        fields = '__all__'
