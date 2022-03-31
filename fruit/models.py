b96a23b0f8ac0ee3e34ebf63e9315a435da9020d
DeysiLopes <deysilinconl@gmail.com>
2022-03-30 21:07:03 -0300

    Create model fruit

--- fruit/models.py
+++ fruit/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from uuid import uuid4
 # Create your models here.
-class fruit(models.Model):
-    
+class Fruit(models.Model):
+    id_fruit = models.UUIDFIELD(primary_key = true, default = uuid4, ediTable = false)
+    name_fruit = models.CharField(max_length=255)
